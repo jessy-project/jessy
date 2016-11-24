50518a6273bd40839e9260f7a6ea6f4a1e2736e5
Bo Maryniuk <bo@maryniuk.net>
2016-11-24 20:02:58 +0000

    Initial slit of STT engines

--- jessy/stt/__init__.py
+++ jessy/stt/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8-*-
 from abc import ABCMeta, abstractmethod
-import jasperpath
-import vocabcompiler
+
+from jessy import jasperpath
+from jessy import vocabcompiler
 
 
 class AbstractSTTEngine(object):
@@ -49,3 +50,42 @@ class AbstractSTTEngine(object):
     def transcribe(self, fp):
         pass
 
+
+def get_engine_by_slug(slug=None):
+    """
+    Returns:
+        An STT Engine implementation available on the current platform
+
+    Raises:
+        ValueError if no speaker implementation is supported on this platform
+    """
+
+    if not slug or type(slug) is not str:
+        raise TypeError("Invalid slug '%s'", slug)
+
+    selected_engines = filter(lambda engine: hasattr(engine, "SLUG") and
+                              engine.SLUG == slug, get_engines())
+    if len(selected_engines) == 0:
+        raise ValueError("No STT engine found for slug '%s'" % slug)
+    else:
+        if len(selected_engines) > 1:
+            print(("WARNING: Multiple STT engines found for slug '%s'. " +
+                   "This is most certainly a bug.") % slug)
+        engine = selected_engines[0]
+        if not engine.is_available():
+            raise ValueError(("STT engine '%s' is not available (due to " +
+                              "missing dependencies, missing " +
+                              "dependencies, etc.)") % slug)
+        return engine
+
+
+def get_engines():
+    def get_subclasses(cls):
+        subclasses = set()
+        for subclass in cls.__subclasses__():
+            subclasses.add(subclass)
+            subclasses.update(get_subclasses(subclass))
+        return subclasses
+    return [tts_engine for tts_engine in
+            list(get_subclasses(AbstractSTTEngine))
+            if hasattr(tts_engine, 'SLUG') and tts_engine.SLUG]
