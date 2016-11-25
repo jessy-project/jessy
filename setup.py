6aed847e6f521253dfff042677c92d8a48f16b13
Bo Maryniuk <bo@suse.de>
2016-11-25 23:43:46 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jessy',
+    version='',
+    packages=['jessy', 'jessy.stt', 'jessy.tts', 'jessy.utils', 'jessy.modules'],
+    url='https://github.com/isbm/jessy',
+    license='MIT',
+    author='Bo Maryniuk',
+    author_email='bo@suse.de',
+    description='Jessy is an open source platform for developing always-on, voice-controlled applications.'
+)
