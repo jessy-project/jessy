8780e3dff11989228363606298cae0c31f7fd1be
Bo Maryniuk <bo@maryniuk.net>
2016-11-26 13:57:35 +0000

    Fix static paths

--- jessy/jasperpath.py
+++ jessy/jasperpath.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8-*-
 import os
+import jessy
 
-# Jasper main directory
-APP_PATH = os.path.normpath(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), os.pardir))
-
-DATA_PATH = os.path.join(APP_PATH, "static")
-LIB_PATH = os.path.join(APP_PATH, "jessy")
+APP_PATH = LIB_PATH = jessy.__path__[0]
+DATA_PATH = os.path.join(os.environ.get('VIRTUAL_ENV', '/usr'), "share/jessy/static")
 PLUGIN_PATH = os.path.join(LIB_PATH, "modules")
-
-CONFIG_PATH = os.path.expanduser(os.getenv('JASPER_CONFIG', '~/.jasper'))
+CONFIG_PATH = os.path.expanduser(os.getenv('JASPER_CONFIG', '{0}/.jessy'.format(os.path.expanduser('~'))))
 
 
 def config(*fname):
+    '''
+    Get config path.
+    '''
     return os.path.join(CONFIG_PATH, *fname)
 
 
 def data(*fname):
+    '''
+    Get path of the static data
+    '''
     return os.path.join(DATA_PATH, *fname)
