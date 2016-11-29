eb0a362ee009a7143c41deea0f6dbfc95a62e03f
Bo Maryniuk <bo@suse.de>
2016-11-29 21:48:32 +0100

    Control mplayer through the FIFO

--- jessy/modules/_mplayer/mpwrapper.py
+++ jessy/modules/_mplayer/mpwrapper.py
@@ -25,7 +25,8 @@
 import os
 import signal
 import subprocess
-import time
+import shutil
+import tempfile
 
 
 class MPlayer(object):
@@ -33,9 +34,21 @@ class MPlayer(object):
     MPlayer wrapper.
     '''
     def __init__(self):
-        self._cmd = 'mplayer -really-quiet {0}'
+        self._cmd = 'mplayer -really-quiet -slave -input file={fifo} {url}'
         self._station = ''
         self._process = None
+        self._fifo = None
+        self._volume = 100
+
+    def _volume(method):
+        '''
+        Volume decorator.
+        '''
+        def postprocess(self):
+            method(self)
+            print "Setting volume to", self._volume, "at", id(self)
+            self._fifo.write('volume {0} 1\n'.format(self._volume))
+        return postprocess
 
     def _cleanup(method):
         '''
@@ -44,7 +57,43 @@ class MPlayer(object):
         def postprocess(self):
             method(self)
             self._process = None
+            self._remove_fifo()
         return postprocess
+
+    def _create_fifo(self):
+        '''
+        Create a fifo file for controlling MPlayer.
+        '''
+        if not self._fifo:
+            fifo_path = os.path.join(tempfile.mkdtemp(), '.mplayer.fifo')
+            try:
+                os.mkfifo(fifo_path)
+                self._fifo = open(fifo_path, 'w+', 0)
+            except OSError as ex:
+                print "Error creating control fifo"
+
+        return self._fifo.name
+
+    def _remove_fifo(self):
+        '''
+        Remove fifo
+        '''
+        if self._fifo:
+            self._fifo.close()
+            shutil.rmtree(os.path.dirname(self._fifo.name))
+            self._fifo = None
+
+    def _is_running(self):
+        '''
+        Check if process is running.
+        '''
+        if self._process:
+            print "Check:", self._process.pid
+            try:
+                os.kill(self._process.pid, 0)
+            except OSError as ex:
+                return False
+        return True
 
     @property
     def station(self):
@@ -55,14 +104,19 @@ class MPlayer(object):
         if value.startswith('http'):
             self._station = value
 
+    @_volume
     def play(self):
         if self._process is None:
             if self.station:
-                self._process = subprocess.Popen(self._cmd.format(self.station),
-                                                 stdin=subprocess.PIPE,
-                                                 stdout=open('/dev/null', 'w'),
-                                                 stderr=open('/dev/null', 'w'),
-                                                 shell=True, preexec_fn=os.setsid)
+                fifo_path = self._create_fifo()
+                if fifo_path:
+                    self._process = subprocess.Popen(self._cmd.format(fifo=fifo_path, url=self.station),
+                                                     stdin=subprocess.PIPE,
+                                                     stdout=open('/dev/null', 'w'),
+                                                     stderr=open('/dev/null', 'w'),
+                                                     shell=True, preexec_fn=os.setsid)
+                    if self._is_running():
+                        self._volume = 100
             else:
                 print "Error: no station selected"
         else:
@@ -71,7 +125,36 @@ class MPlayer(object):
     @_cleanup
     def stop(self):
         if self._process is not None:
-            self._process.communicate('q')
+            self._fifo.write('stop\n')
+
+    @_volume
+    def quieter(self):
+        if self._volume - 25 < 0:
+            self._volume = 0
+        else:
+            self._volume -= 25
+
+    @_volume
+    def louder(self):
+        if self._volume + 25 < 100:
+            self._volume += 25
+        else:
+            self._volume = 100
+
+    @_volume
+    def background(self):
+        self._volume /= 2
+
+    @_volume
+    def foreground(self):
+        self._volume *= 2
+
+    def mute(self):
+        self._fifo.write('volume 0 1\n')
+
+    @_volume
+    def unmute(self):
+        pass
 
     @_cleanup
     def kill(self):
