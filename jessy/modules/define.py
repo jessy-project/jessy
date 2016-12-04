f5fbd4942ebdcec2be1410b3750452e7febf1cdd
Bo Maryniuk <bo@suse.de>
2016-12-04 21:13:11 +0100

    Get short answers by duckduckgo

--- jessy/modules/define.py
+++ jessy/modules/define.py
@@ -22,19 +22,85 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from jessy.modules import JessyModule, all_words
-import _ddg as duck
+from jessy.modules import JessyModule
+from jessy.modules import _ddg as duck
+import time
 
 
 class DefineWord(JessyModule):
     '''
     Plugin for word definition.
     '''
+    NUMBERS = {
+        1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+        6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+    }
+
+    ORDERS = {
+        1: 'first', 2: 'second', 3: 'third', 4: 'fourth', 5: 'fifth',
+        6: 'sixth', 7: 'seventh', 8: 'eighth', 9: 'nineth', 10: 'last',
+    }
+
     def __init__(self, *args, **kwargs):
         JessyModule.__init__(self, *args, **kwargs)
 
+    def _extract_definition(self, text):
+        '''
+        Extracts definition from the given text
+
+        :param text:
+        :return:
+        '''
+        text = text.lower()
+        definition = ''
+        for kwd in self.keywords():
+            if kwd in text:
+                definition = text.split(kwd)[-1].split(' ')[-1]
+                if definition:
+                    break
+
+        return definition
+
+    def _get_answer(self, definition):
+        '''
+        Get answer.
+
+        :param definition:
+        :return:
+        '''
+        result = duck.query(definition)
+        if result.type == 'disambiguation' and  result.related:
+            related = [res for res in result.related if hasattr(res, 'text') and not res.text.endswith('...')][:5]
+            self.say('There {0} {1} definition{2} for this {3}'.format(
+                (len(related) - 1) and 'are' or 'is',
+                self.NUMBERS[len(related)],
+                (len(related) - 1) and 's' or '',
+                result.type))
+
+            for r_num, r_data in enumerate(related):
+                self.say("{}.".format(self.ORDERS[r_num + 1]))
+                time.sleep(1)
+                self.say(r_data.text.encode('UTF-8', 'ignore'))
+                time.sleep(1)
+
+        self.say('I hope this helps')
+
+    def _handle(self, text):
+        '''
+        Main handler.
+
+        :param text:
+        :return:
+        '''
+        definition = self._extract_definition(text)
+        if definition:
+            self._mic.say('Let me look...')
+            answer = self._get_answer(definition)  # Should go background before saying 'please wait'
+
     def handle(self, transcription):
-        pass
+        if self.matches(transcription):
+            self._handle(transcription)
+            return True
 
     @classmethod
     def keywords(cls):
